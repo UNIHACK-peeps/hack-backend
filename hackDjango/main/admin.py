6e8906086cdc629749c61abd4151fbd4c8cf54a1
Richard <richard_h199@hotmail.com>
2018-07-28 17:45:12 +1000

    add to admin

--- hackDjango/main/admin.py
+++ hackDjango/main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from main.models import AppUser
+from main.models import AppUser,Request,Subject
 # Register your models here.
 admin.site.register(AppUser)
+admin.site.register(Request)
+admin.site.register(Subject)
--- hackDjango/main/models.py
+++ hackDjango/main/models.py
@@ -10,34 +10,38 @@ SUBJECTS = (("MAT", "Mathematics"),
 
 class AppUser(models.Model):
     name = models.CharField(max_length=200)
-    subjectsOffered = models.ManyToManyField("Subject")
+    subjectsOffered = models.ManyToManyField("Subject",related_name="subjectsOffered")
 
     def acceptRequest(self, request_id):
         Request[request_id].acceptedTutors_set.add(self)
         
     def acceptTutor(self, request_id, tutor_id):
         currRequest = Request.objects.get(id=request_id)
-        acceptedTutor = Tutor.objects.get(id=tutor_id)
+        acceptedTutor = AppUser.objects.get(id=tutor_id)
         currRequest.chosenTutor = acceptedTutor
-        
+    def __str__(self):
+        return self.name
     
 class Request(models.Model):
     tutee = models.OneToOneField("AppUser", on_delete=models.CASCADE, related_name="tutee")
     requestedSubject = models.OneToOneField("Subject", on_delete=models.CASCADE,
                                             related_name="requestedSubject")
-    avaliableTutors = models.ManyToManyField("AppUser", related_name="avaliableTutors", null=True)
-    acceptedTutors = models.ManyToManyField("AppUser", related_name="acceptedTutors", null=True)
+    avaliableTutors = models.ManyToManyField("AppUser", related_name="avaliableTutors", blank=True)
+    acceptedTutors = models.ManyToManyField("AppUser", related_name="acceptedTutors",blank=True,default=None)
     chosenTutor = models.OneToOneField("AppUser", on_delete=models.CASCADE,
-                                       related_name="chosenTutor", null=True)
+                                       related_name="chosenTutor", null=True,blank = True)
     
     def computeAvaliableTutors(self, subject_id, frequency):
         """
         Telling the tutors what requests are for them
         """
+        subject = Subject.objects.get(id=subject_id)
         for tutor in AppUser.objects.all():
-            if subject in tutor.subjectsOffered_set:
+            if subject in tutor.subjectsOffered.all():
                 avaliableTutors.add(tutor)
 
     
 class Subject(models.Model):
     name = models.CharField(max_length=3, choices=SUBJECTS)
+    def __str__(self):
+        return self.name
